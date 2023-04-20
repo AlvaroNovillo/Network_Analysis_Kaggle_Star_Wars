e4fcd3dd307e44d352e446431225a47ca340a496
AlvaroNovillo <alvanovi@ucm.es>
2023-04-20 18:55:23 +0200

    Top tier included

--- main_code.R
+++ main_code.R
@@ -59,6 +59,28 @@ farthest.nodes(network, weights = E(network)$weight)
 geodesic_distance <- shortest.paths(network)
 
 get.diameter(network)
+
+degree(network)
+plot(table(degree(network)))
+
+cat("Average degree:", mean(degree(network)), "\n")
+
+
+# Top ten characters with the most screenplay
+# Assuming you have a dataframe named 'nodes'
+# Subset the dataframe to include only the top characters based on 'value'
+top_nodes <- nodes[order(nodes$value, decreasing = TRUE),][1:10,]
+
+print(top_nodes)
+# Create a barplot using 'name.real' as labels and 'value' as heights
+barplot(top_nodes$value, 
+        names.arg = top_nodes$name.real, 
+        main = "Top Characters", 
+        xlab = "Character Name", 
+        ylab = "Nodes Value", 
+        col = "steelblue")
+
+
 #################Adjacency matrix #############################
 # Get the adjacency matrix and convert to long format
 adj_matrix <- as_adjacency_matrix(network,attr = "weight",sparse = T)
@@ -74,7 +96,7 @@ corrplot(as.matrix(adj_matrix),  method = 'shade',is.corr = FALSE,col.lim = c(0,
 library(chorddiag)
 chorddiag(as.matrix(adj_matrix), groupColors = nodes$colour,showTicks = FALSE, margin = 170, groupnameFontsize = 12)
 ################### Subgraphs of different movies ################
-subnetwork1 <- subgraph.edges(network, E(network)[E(network)$weight>max(E(network)$weight)*0.5]) #0.5 2 subgraphs, 0.3 ep 7 and ep 1-6, 0.2 main characters
+subnetwork1 <- subgraph.edges(network, E(network)[E(network)$weight>max(E(network)$weight)*0.95]) #0.5 2 subgraphs, 0.3 ep 7 and ep 1-6, 0.2 main characters
 m2 <- layout_nicely(subnetwork1)
 #m2 <- layout_as_tree(subnetwork1)
 plot(subnetwork1,edge.color = 'grey',layout = m2,edge.width = (edges$value - min(edges$value))/(max(edges$value) - min(edges$value))*(7-2) + 2 , vertex.size=(nodes$value - min(nodes$value))/(max(nodes$value) - min(nodes$value))*(20-5) + 7)
